555d61ea26284623937725be2881fb13793d8daa
Paul Christian Prieto <pcmprieto@gmail.com>
2020-06-24 15:27:43 +0800

    Fix: replace quantity dropdown with input box with buttons + and -

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,22 @@
 $(document).ready(function () {
 
+    // Input for numbers 
+
+    $('.minus').click(function () {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $('.plus').click(function () {
+        var $input = $(this).parent().find('input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
+    });
+
     // OPEN CART MODAL
     $('#cartModal').click(function () {
         $('.modal').addClass('active');
