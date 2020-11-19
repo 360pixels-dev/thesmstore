8e51a0640597a522e33177e35352977593e01ca7
Arnel Almazan <arnel@360pixels.co>
2020-11-19 10:26:22 +0800

    fixed mobile gift carousel image aspect ratio

--- assets/js/newmobilecarousel.js
+++ assets/js/newmobilecarousel.js
@@ -1,3 +1,5 @@
+// MOBILE CAROUSELS
+// GIFT SECTION
 $(document).ready(() => {
     $('.mobile-carousel-gift-section__cards').slick({
         dots: false,
@@ -23,3 +25,21 @@ $(document).ready(() => {
     });
 })
 
+// SM CARES
+$(document).ready(() => {
+    $('.mobile-sm-cares__cards').slick({
+        arrows: true,
+        mobileFirst: true,
+        prevArrow: '<button class="mobile-sm-cares__controllers--left" href=""> <img src="./assets/images/icons/m-carousel-arrow-left.svg" alt=""></button>',
+        nextArrow: '<button class="mobile-sm-cares__controllers--right" href=""><img src="./assets/images/icons/m-carousel-arrow-right.svg" alt=""></button>',
+        appendArrows: $('.mobile-sm-cares__controllers').html("")
+    });
+
+    $('.leftArrow').on('click', () => {
+        $('.mobile-sm-cares__cards').slickPrev();
+    });
+    
+    $('.rightArrow').on('click', () => {
+        $('.mobile-sm-cares__cards').slickNext();
+    });
+})
