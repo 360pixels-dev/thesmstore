b39f2a487569446cc55e4f1b2a066a507ebce469
Arnel Almazan <arnel@360pixels.co>
2020-11-26 12:40:34 +0800

    fixed box sizes carousel

--- assets/js/horizontal-carousel.js
+++ assets/js/horizontal-carousel.js
@@ -1,10 +1,10 @@
-$(document).ready(function(){
-  $('.box-sizes-slider__slider').slick({
-    slidesToShow: 4,
-    dots: true,
-    arrows: true
-  });
-});
+// $(document).ready(function(){
+//   $('.box-sizes-slider__slider').slick({
+//     slidesToShow: 4,
+//     dots: true,
+//     arrows: true
+//   });
+// });
 
 $(document).ready(() => {
   $('.events-slider__slider').slick({
@@ -35,16 +35,16 @@ $(document).ready(() => {
   $('.box-sizes__slider-slides').slick({
       dots: false,
       infinite: true,
-      adaptiveWidth: true,
       speed: 300,
       slidesToShow: 3,
       slidesToScroll: 1,
       arrows: true,
       mobileFirst: true,
+      focusOnSelect: true,
       centerMode: false,
-      prevArrow: '<button class="controls__arrow controls__arrow--left"><img src="./assets/images/arrow-tail-left.svg" alt=""></button>',
-      nextArrow: '<button class="controls__arrow controls__arrow--right"><img src="./assets/images/arrow-tail-right.svg" alt=""></button>',
-      appendArrows: $('.box-sizes__controls').html("")
+      // prevArrow: '<button class="controls__arrow controls__arrow--left"><img src="./assets/images/arrow-tail-left.svg" alt=""></button>',
+      // nextArrow: '<button class="controls__arrow controls__arrow--right"><img src="./assets/images/arrow-tail-right.svg" alt=""></button>',
+      // appendArrows: $('.box-sizes__controls').html("")
   });
 
   // $('.leftArrow').on('click', () => {
