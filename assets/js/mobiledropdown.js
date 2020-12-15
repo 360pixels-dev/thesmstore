f8d9ecf36c404270dfbaf4dbab10a170d18d3bc7
Arnel Almazan <arnel@360pixels.co>
2020-12-15 13:51:53 +0800

    refactored Phase 1 mobile nav

--- assets/js/mobiledropdown.js
+++ assets/js/mobiledropdown.js
@@ -39,123 +39,280 @@ function closeMobileMenu() {
     }
 }
 
-function showHideMenu(ele) {
-    let menu = document.getElementById(ele);
+function toggleMobileMenu() {
+    let menu = document.getElementById('mobile-menu')
     let menuOverlay = document.getElementById('mobile-menu-overlay')
-    let menuList = document.getElementById('mobile-menu-list')
-    let menus = document.getElementsByClassName('mobile-menu-item')
-    // let menuArrow = document.getElementById("mobile-menu-arrow")
 
     if (menu !== null) {
         if (menu.style.display === "block") {
-            // menuArrow.className = "mobile-menu-arrow"
             menu.style.display = 'none'
             menuOverlay.style.display = 'none'
-            menuList.style.display = 'block'
-            for (let i = 0; i < menus.length; i++) {
-                menus[i].style.display = 'none'
-            }
-        }
-        else {
+        } else {
             menu.style.display = 'block'
             menuOverlay.style.display = 'block'
-            // menuArrow.className = "mobile-menu-arrow active"
         }
-        return false;
     }
 }
 
-function showHideSubMenu(ele) {
-    let selectedMenu = document.getElementById(ele);
-
-    let menuList = document.getElementById('mobile-menu-list')
-    let menus = document.getElementsByClassName('mobile-menu-item')
-    let categoriesList = document.getElementById('mobile-menu-categories-list')
-    let servicesList = document.getElementById('mobile-menu-services-list')
-    let whatsnewList = document.getElementById('mobile-menu-whatsnew-list')
-
-    let categoriesFashion = document.getElementById('mobile-menu-categories-fashion')
-    let categoriesHome = document.getElementById('mobile-menu-categories-home')
-    let categoriesBnk = document.getElementById('mobile-menu-categories-bnk')
-    let categoriesBeauty = document.getElementById('mobile-menu-categories-beauty')
-    let categoriesOthers = document.getElementById('mobile-menu-categories-others')
-
-    let whatsnewPromos = document.getElementById('mobile-menu-whatsnew-promos')
-
-    switch (ele) {
-        case 'mobile-menu-list':
-            for (let i = 0; i < menus.length; i++) {
-                menus[i].style.display = 'none'
-            }
-            menuList.style.display = 'block'
-            break;
+function toggleCategoriesSubMenu() {
+    let mainMenu = document.getElementById('mobile-menu-list')
+    let menu = document.getElementById('mobile-menu-categories-list')
     
-        case 'mobile-menu-categories-list':
-            for (let i = 0; i < menus.length; i++) {
-                menus[i].style.display = 'none'
-            }
-            // menuList.style.display = 'none'
-            categoriesList.style.display = 'block'
-            break;
-    
-        case 'mobile-menu-services-list':
-            // menuList.style.display = 'none'
-            servicesList.style.display = 'block'
-            break;
-    
-        case 'mobile-menu-whatsnew-list':
-            // menuList.style.display = 'none'
-            whatsnewPromos.style.display = 'none'
-            whatsnewList.style.display = 'block'
-            break;
-    
-        case 'mobile-menu-categories-fashion':
-            for (let i = 0; i < menus.length; i++) {
-                menus[i].style.display = 'none'
-            }
-            // menuList.style.display = 'none'
-            categoriesFashion.style.display = 'block'
-            break;
-    
-        case 'mobile-menu-categories-home':
-            for (let i = 0; i < menus.length; i++) {
-                menus[i].style.display = 'none'
-            }
-            // menuList.style.display = 'none'
-            categoriesHome.style.display = 'block'
-            break;
-    
-        case 'mobile-menu-categories-bnk':
-            for (let i = 0; i < menus.length; i++) {
-                menus[i].style.display = 'none'
-            }
-            // menuList.style.display = 'none'
-            categoriesBnk.style.display = 'block'
-            break;
-    
-        case 'mobile-menu-categories-beauty':
-            for (let i = 0; i < menus.length; i++) {
-                menus[i].style.display = 'none'
-            }
-            // menuList.style.display = 'none'
-            categoriesBeauty.style.display = 'block'
-            break;
-    
-        case 'mobile-menu-categories-others':
-            for (let i = 0; i < menus.length; i++) {
-                menus[i].style.display = 'none'
-            }
-            // menuList.style.display = 'none'
-            categoriesOthers.style.display = 'block'
-            break;
-    
-        case 'mobile-menu-whatsnew-promos':
-            // menuList.style.display = 'none'
-            // whatsnewList.style.display = 'none'
-            whatsnewPromos.style.display = 'block'
-            break;
-    
-        default:
-            break;
+    if (menu !== null) {
+        if (menu.style.display === "block") {
+            menu.style.display = 'none'
+            mainMenu.style.display = 'block'
+        } else {
+            menu.style.display = 'block'
+            mainMenu.style.display = 'none'
+        }
     }
 }
+
+function toggleFashionSubMenu() {
+    let categoriesMenu = document.getElementById('mobile-menu-categories-list')
+    let menu = document.getElementById('mobile-menu-categories-fashion')
+    
+    if (menu !== null) {
+        if (menu.style.display === "block") {
+            menu.style.display = 'none'
+            categoriesMenu.style.display = 'block'
+
+        } else {
+            menu.style.display = 'block'
+            categoriesMenu.style.display = 'none'
+        }
+    }
+}
+
+function toggleBnkSubMenu() {
+    let categoriesMenu = document.getElementById('mobile-menu-categories-list')
+    let menu = document.getElementById('mobile-menu-categories-bnk')
+    
+    if (menu !== null) {
+        if (menu.style.display === "block") {
+            menu.style.display = 'none'
+            categoriesMenu.style.display = 'block'
+
+        } else {
+            menu.style.display = 'block'
+            categoriesMenu.style.display = 'none'
+        }
+    }
+}
+
+function toggleHomeSubMenu() {
+    let categoriesMenu = document.getElementById('mobile-menu-categories-list')
+    let menu = document.getElementById('mobile-menu-categories-home')
+    
+    if (menu !== null) {
+        if (menu.style.display === "block") {
+            menu.style.display = 'none'
+            categoriesMenu.style.display = 'block'
+
+        } else {
+            menu.style.display = 'block'
+            categoriesMenu.style.display = 'none'
+        }
+    }
+}
+
+function toggleBeautySubMenu() {
+    let categoriesMenu = document.getElementById('mobile-menu-categories-list')
+    let menu = document.getElementById('mobile-menu-categories-beauty')
+    
+    if (menu !== null) {
+        if (menu.style.display === "block") {
+            menu.style.display = 'none'
+            categoriesMenu.style.display = 'block'
+
+        } else {
+            menu.style.display = 'block'
+            categoriesMenu.style.display = 'none'
+        }
+    }
+}
+
+function toggleOthersSubMenu() {
+    let categoriesMenu = document.getElementById('mobile-menu-categories-list')
+    let menu = document.getElementById('mobile-menu-categories-others')
+    
+    if (menu !== null) {
+        if (menu.style.display === "block") {
+            menu.style.display = 'none'
+            categoriesMenu.style.display = 'block'
+
+        } else {
+            menu.style.display = 'block'
+            categoriesMenu.style.display = 'none'
+        }
+    }
+}
+
+function toggleServicesSubMenu() {
+    let mainMenu = document.getElementById('mobile-menu-list')
+    let menu = document.getElementById('mobile-menu-services-list')
+    
+    if (menu !== null) {
+        if (menu.style.display === "block") {
+            menu.style.display = 'none'
+            mainMenu.style.display = 'block'
+        } else {
+            menu.style.display = 'block'
+            mainMenu.style.display = 'none'
+        }
+    }
+}
+
+function toggleWhatsNewSubMenu() {
+    let mainMenu = document.getElementById('mobile-menu-list')
+    let menu = document.getElementById('mobile-menu-whatsnew-list')
+    
+    if (menu !== null) {
+        if (menu.style.display === "block") {
+            menu.style.display = 'none'
+            mainMenu.style.display = 'block'
+        } else {
+            menu.style.display = 'block'
+            mainMenu.style.display = 'none'
+        }
+    }
+}
+
+function togglePromosSubMenu() {
+    let whatsNewMenu = document.getElementById('mobile-menu-whatsnew-list')
+    let menu = document.getElementById('mobile-menu-whatsnew-promos')
+
+    console.log(menu.style.display)
+    
+    if (menu !== null) {
+        if (menu.style.display === "block") {
+            whatsNewMenu.style.display = 'block'
+            menu.style.display = 'none'
+        } else {
+            whatsNewMenu.style.display = 'none'
+            menu.style.display = 'block'
+        }
+    }
+}
+
+// function showHideMenu(ele) {
+//     let menu = document.getElementById(ele);
+//     let menuOverlay = document.getElementById('mobile-menu-overlay')
+//     let menuList = document.getElementById('mobile-menu-list')
+//     let menus = document.getElementsByClassName('mobile-menu-item')
+//     // let menuArrow = document.getElementById("mobile-menu-arrow")
+
+//     if (menu !== null) {
+//         if (menu.style.display === "block") {
+//             // menuArrow.className = "mobile-menu-arrow"
+//             menu.style.display = 'none'
+//             menuOverlay.style.display = 'none'
+//             menuList.style.display = 'block'
+//             for (let i = 0; i < menus.length; i++) {
+//                 menus[i].style.display = 'none'
+//             }
+//         }
+//         else {
+//             menu.style.display = 'block'
+//             menuOverlay.style.display = 'block'
+//             // menuArrow.className = "mobile-menu-arrow active"
+//         }
+//         return false;
+//     }
+// }
+
+// function showHideSubMenu(ele) {
+//     let selectedMenu = document.getElementById(ele);
+
+//     let menuList = document.getElementById('mobile-menu-list')
+//     let menus = document.getElementsByClassName('mobile-menu-item')
+//     let categoriesList = document.getElementById('mobile-menu-categories-list')
+//     let servicesList = document.getElementById('mobile-menu-services-list')
+//     let whatsnewList = document.getElementById('mobile-menu-whatsnew-list')
+
+//     let categoriesFashion = document.getElementById('mobile-menu-categories-fashion')
+//     let categoriesHome = document.getElementById('mobile-menu-categories-home')
+//     let categoriesBnk = document.getElementById('mobile-menu-categories-bnk')
+//     let categoriesBeauty = document.getElementById('mobile-menu-categories-beauty')
+//     let categoriesOthers = document.getElementById('mobile-menu-categories-others')
+
+//     let whatsnewPromos = document.getElementById('mobile-menu-whatsnew-promos')
+
+//     switch (ele) {
+//         case 'mobile-menu-list':
+//             for (let i = 0; i < menus.length; i++) {
+//                 menus[i].style.display = 'none'
+//             }
+//             menuList.style.display = 'block'
+//             break;
+    
+//         case 'mobile-menu-categories-list':
+//             for (let i = 0; i < menus.length; i++) {
+//                 menus[i].style.display = 'none'
+//             }
+//             // menuList.style.display = 'none'
+//             categoriesList.style.display = 'block'
+//             break;
+    
+//         case 'mobile-menu-services-list':
+//             // menuList.style.display = 'none'
+//             servicesList.style.display = 'block'
+//             break;
+    
+//         case 'mobile-menu-whatsnew-list':
+//             // menuList.style.display = 'none'
+//             whatsnewPromos.style.display = 'none'
+//             whatsnewList.style.display = 'block'
+//             break;
+    
+//         case 'mobile-menu-categories-fashion':
+//             for (let i = 0; i < menus.length; i++) {
+//                 menus[i].style.display = 'none'
+//             }
+//             // menuList.style.display = 'none'
+//             categoriesFashion.style.display = 'block'
+//             break;
+    
+//         case 'mobile-menu-categories-home':
+//             for (let i = 0; i < menus.length; i++) {
+//                 menus[i].style.display = 'none'
+//             }
+//             // menuList.style.display = 'none'
+//             categoriesHome.style.display = 'block'
+//             break;
+    
+//         case 'mobile-menu-categories-bnk':
+//             for (let i = 0; i < menus.length; i++) {
+//                 menus[i].style.display = 'none'
+//             }
+//             // menuList.style.display = 'none'
+//             categoriesBnk.style.display = 'block'
+//             break;
+    
+//         case 'mobile-menu-categories-beauty':
+//             for (let i = 0; i < menus.length; i++) {
+//                 menus[i].style.display = 'none'
+//             }
+//             // menuList.style.display = 'none'
+//             categoriesBeauty.style.display = 'block'
+//             break;
+    
+//         case 'mobile-menu-categories-others':
+//             for (let i = 0; i < menus.length; i++) {
+//                 menus[i].style.display = 'none'
+//             }
+//             // menuList.style.display = 'none'
+//             categoriesOthers.style.display = 'block'
+//             break;
+    
+//         case 'mobile-menu-whatsnew-promos':
+//             // menuList.style.display = 'none'
+//             // whatsnewList.style.display = 'none'
+//             whatsnewPromos.style.display = 'block'
+//             break;
+    
+//         default:
+//             break;
+//     }
+// }
