59a4d7c8f55394143e27f9cfb832370490eb4028
Arnel Almazan <arnel@360pixels.co>
2021-02-18 20:13:51 +0800

    magazine reader cleanup

---
+++ assets/js/magazine-reader.js
@@ -0,0 +1,10 @@
+
+if ($('#book_pdf').length !== 0) {
+    var option_book_pdf = {
+        source:'assets/sample.pdf',
+        webgl:true,
+        webglShadow:true,
+        // openPage:2,
+        height: 852
+    };
+}
